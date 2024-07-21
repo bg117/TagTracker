6f5da432433f694280e150539b74b86e35d418c8
bg117 <76200573+bg117@users.noreply.github.com>
2024-07-21 20:09:17 +0800

    Refactor MainWindowViewModel to dynamically update SerialPorts and BaudRates
    Signed-off-by: bg117 <76200573+bg117@users.noreply.github.com>

--- TagTracker/Program.cs
+++ TagTracker/Program.cs
@@ -1,16 +1,23 @@
-﻿using Avalonia;
-using System;
+﻿using System;
+using Avalonia;
+using Usb.Events;
 
 namespace TagTracker;
 
-sealed class Program
+internal sealed class Program
 {
+    internal static IUsbEventWatcher UsbEventWatcher = new UsbEventWatcher();
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+        UsbEventWatcher.Dispose();
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
--- TagTracker/ViewModels/MainWindowViewModel.cs
+++ TagTracker/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,46 @@
-﻿namespace TagTracker.ViewModels;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using TagTracker.Models;
 
-public partial class MainWindowViewModel : ViewModelBase
+namespace TagTracker.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private readonly SerialPortModel _serialPortModel = new();
+
+    private int _selectedBaudRateIndex;
+
+    private int _selectedSerialPortIndex;
+
+    public MainWindowViewModel()
+    {
+        _serialPortModel.PropertyChanged += OnSerialPortsChanged;
+    }
+
+    public IEnumerable<string> SerialPorts => _serialPortModel.SerialPorts;
+
+    public IEnumerable<int> BaudRates =>
+    [
+        110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600,
+        115200
+    ];
+
+    public int SelectedSerialPortIndex
+    {
+        get => _selectedSerialPortIndex;
+        set => SetProperty(ref _selectedSerialPortIndex, value);
+    }
+
+    public int SelectedBaudRateIndex
+    {
+        get => _selectedBaudRateIndex;
+        set => SetProperty(ref _selectedBaudRateIndex, value);
+    }
+
+    private void OnSerialPortsChanged(object? sender,
+        PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(_serialPortModel.SerialPorts))
+            OnPropertyChanged(nameof(SerialPorts));
+    }
 }
