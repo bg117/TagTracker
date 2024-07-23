9c54ef1c94422e1de3dfa25be7bd389ee8894c53
bg117 <76200573+bg117@users.noreply.github.com>
2024-07-23 20:54:35 +0800

    Separate Tag model classes into their own files
    Signed-off-by: bg117 <76200573+bg117@users.noreply.github.com>

--- TagTracker/Models/TagModel.cs
+++ TagTracker/Models/TagModel.cs
@@ -1,7 +1,4 @@
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.IO;
-using Microsoft.EntityFrameworkCore;
 
 namespace TagTracker.Models;
 
@@ -13,25 +10,4 @@ public class Tag
     public string Uid      { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
     public string Lrn      { get; set; } = string.Empty;
-}
-
-public sealed class TagContext : DbContext
-{
-    public TagContext()
-    {
-        var local  = Environment.SpecialFolder.LocalApplicationData;
-        var folder = Environment.GetFolderPath(local);
-        DatabasePath = Path.Combine(folder, "tags.db");
-        Database.EnsureCreated();
-    }
-
-    public  DbSet<Tag> Tags         { get; set; } = default!;
-    private string     DatabasePath { get; }
-
-    protected override void OnConfiguring(
-        DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlite($"Data Source={DatabasePath}");
-    }
 }
